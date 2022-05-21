f90529a02ca2ff6db6f0dc5024128647295d1c0e
Kathan0511 <77751524+Kathan0511@users.noreply.github.com>
2022-05-21 16:31:40 -0400

    Add files via upload

---
+++ Workshop3/Pair.cpp
@@ -0,0 +1,32 @@
+
+
+
+#include "Pair.h"
+
+using namespace std;
+
+
+namespace sdds
+{
+
+    bool Pair::haveSameKey(const Pair& L, const Pair& M) {
+        bool res = false;
+        if (L.m_key == M.m_key) {
+            res = true;
+        }
+        return res;
+    }
+
+    std::ostream& Pair::display(std::ostream& ostr) const {
+        ostr.width(20);
+        ostr.setf(ios::right);
+        ostr << getKey();
+        ostr.setf(ios::left);
+        ostr << ": " << getValue();
+        return ostr;
+    }
+
+    std::ostream& operator<<(std::ostream& ostr, const Pair& L) {
+        return L.display(ostr);
+    }
+}
